0c1491af570ae6d92bb71bb678aa26f21126a307
Egor Paroshin <eparoshin@gmail.com>
2025-03-22 17:17:21 +0300

    fix warnings

--- XMonad/Layout/Bayan.hs
+++ XMonad/Layout/Bayan.hs
@@ -1,23 +1,22 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE TupleSections #-}
 module XMonad.Layout.Bayan (
-    Bayan(..), handleStack') where
+    Bayan(..)) where
 
 import XMonad
 import qualified XMonad.StackSet as W
-import Data.Maybe
-import Data.Tuple
 
 data Bayan a = Bayan { bayanNMaster :: !Int
                      , bayanMasterIdx :: !Int
-                     , bayanPrevStack :: Maybe (W.Stack Window)
+                     , bayanPrevStack :: !(Maybe (W.Stack Window))
                      }
 
     deriving ( Read, Show )
 
 instance LayoutClass Bayan Window where
     doLayout l' r' s' = return $ doLayout' l' r' s'
-      where makeWindows stack = let ups' = drop 1 . cycle $ (((W.focus stack) : (W.up stack)) ++ (reverse . W.down $ stack))
-                                    downs' = cycle $ (((W.focus stack) : (W.down stack)) ++ (reverse . W.up $ stack))
+      where makeWindows stack = let ups' = drop 1 . cycle $ ((W.focus stack : W.up stack) ++ (reverse . W.down $ stack))
+                                    downs' = cycle ((W.focus stack : W.down stack) ++ (reverse . W.up $ stack))
                                     in (ups', downs')
             (ups, downs) = makeWindows s'
             doLayout' l@(Bayan nMaster masterIdx prevStack) r s
@@ -26,25 +25,25 @@ instance LayoutClass Bayan Window where
                 | otherwise = let newLayout = maybe (l {bayanPrevStack = Just s}) (handleStack s nMaster masterIdx) prevStack
                                   newMasterIdx = bayanMasterIdx newLayout
                                   winStart = (reverse . take newMasterIdx $ ups) ++ downs
-                                  winToDraw = (head winStart) : (takeWhile (/= head winStart) . take (nMaster - 1) $ (tail winStart))
-                                in (flip (,) $ Just newLayout) $ (drawWindows r $ winToDraw)
+                                  winToDraw = head winStart : (takeWhile (/= head winStart) . take (nMaster - 1) $ tail winStart)
+                                in (, Just newLayout) (drawWindows r winToDraw)
 
-            handleStack newStack nMaster masterIdx prevStack = Bayan nMaster (min (nMaster - 1) . max 0 $ compareStacks (0)) (Just newStack)
+            handleStack newStack nMaster masterIdx prevStack = Bayan nMaster (min (nMaster - 1) . max 0 $ compareStacks 0) (Just newStack)
                 where (prevUps, prevDowns) = makeWindows prevStack
                       (_, newDowns) = makeWindows newStack
                       compareStacks idx | idx >= nMaster = masterIdx
                                         | otherwise =
                                             let needTake = masterIdx - idx
-                                                begDown = take (nMaster + 2) $ if (needTake >= 0) then ((reverse . take needTake $ prevUps) ++ prevDowns) else (drop (-needTake) prevDowns)
-                                                begNewDown = take (nMaster + 2) $ newDowns
-                                                    in if (and (zipWith (==) begDown begNewDown))
+                                                begDown = take (nMaster + 2) $ if needTake >= 0 then (reverse . take needTake $ prevUps) ++ prevDowns else drop (-needTake) prevDowns
+                                                begNewDown = take (nMaster + 2) newDowns
+                                                    in if and (zipWith (==) begDown begNewDown)
                                                         then idx
                                                         else compareStacks (idx + 1)
 
             drawWindows r windows' = zip windows' (splitVertically (length windows') r)
 
-    emptyLayout (Bayan nMaster _ _) _ = return ([], Just $ (Bayan nMaster 0 Nothing))
+    emptyLayout (Bayan nMaster _ _) _ = return ([], Just (Bayan nMaster 0 Nothing))
 
     pureMessage l m = fmap incmastern (fromMessage m)
-      where incmastern (IncMasterN d) = l { bayanNMaster = max 1 ((bayanNMaster l) + d) }
+      where incmastern (IncMasterN d) = l { bayanNMaster = max 1 (bayanNMaster l + d) }
 
